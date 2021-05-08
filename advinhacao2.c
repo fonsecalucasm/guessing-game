6ba320b0e5734f9cc8b3673feeac41866f4f754e
Lucas M Fonseca <lmdf.013@gmail.com>
2021-05-08 17:22:38 -0300

    some refactorings

--- advinhacao2.c
+++ advinhacao2.c
@@ -8,6 +8,25 @@ void printWelcomeMessage() {
     printf("******************************************\n");
 }
 
+void printWinnerBanner() {
+    printf("                         \n");
+    printf("    |@@@@|     |####|    \n");
+    printf("    |@@@@|     |####|    \n");
+    printf("    |@@@@|     |####|    \n");
+    printf("    \\@@@@|     |####/    \n");
+    printf("     \\@@@|     |###/    \n");
+    printf("      `@@|_____|##'    \n");
+    printf("           (O)    \n");
+    printf("        .-'''''-.    \n");
+    printf("      .'  * * *  `.    \n");
+    printf("     :  *       *  :    \n");
+    printf("    : ~ A S C I I ~ :    \n");
+    printf("    : ~ A W A R D ~ :    \n");
+    printf("     :  *       *  :    \n");
+    printf("      `.  * * *  .'    \n");
+    printf("        `-.....-'    \n");
+}
+
 /*
 Get the seconds until now and use it as a seed to rand function.
 Then we use return the rest of division for 100 (the number will be between 0 and 99).
@@ -31,18 +50,36 @@ int main() {
 	
     printWelcomeMessage();
 
-    int numeroSecreto = generateSecretNumber();
-    int tentativas = 0;
+    int numeroSecreto = generateSecretNumber();    
     int acertou = 0;
 
     double pontos = 1000;
     double pontosperdidos = 0;
 
-    do {
-        tentativas++;
+    printf("Selecione o nivel de dificuldade\n");
+    printf(" (1) Facil | (2) Medio | (3) Dificil\n");
+    printf("Escolha: ");
+    int dificuldade;
+    scanf("%d", &dificuldade);    
+
+    int rounds = 0;
+    switch (dificuldade) {
+        case 1:
+            rounds = 20;
+            break;
+        case 2:
+            rounds = 15;
+            break;
+        default:
+            rounds = 6;
+            break;
+    }
+
+    int index;
+    for (index = 1; index <= rounds; index++) {
 
         printf("******************************************\n");
-        printf("* Tentativa %d                            *\n", tentativas);
+        printf("* Tentativa %d                            *\n", index);
         printf("* Qual seu chute?                        *\n");
 
         int chute;
@@ -51,7 +88,7 @@ int main() {
 
         if (chute < 0) {
             printf("* Você não pode usar números negativos!   *\n");
-            tentativas--;
+            index--;
             continue;
         }
 
@@ -65,17 +102,19 @@ int main() {
         pontos = pontos - pontosperdidos;
 
         if (acertou) {
-            printf("* Parabéns!! Você acertou!! =D           *\n");
+            printf("* Parabéns!! Você acertou!! =D           *\n");            
+            printResult(index, pontos);
+            printWinnerBanner();
             break;
         } else if (maior) {
-                printf("* Não foi desta vez. Tente mais uma vez!!*\n");
                 printf("* Dica: O número secreto é maior...      *\n");
         } else {
-            printf("* Não foi desta vez. Tente mais uma vez!!*\n");
             printf("* Dica: O número secreto é menor...      *\n");
         }
 
-    } while(acertou == 0);
+    }
     
-    printResult(tentativas, pontos);
+    if (acertou == 0) {
+        printf("Não foi dessa vez!\n");
+    }
 }
